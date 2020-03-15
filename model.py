fa9b8045cc2bfadb3ec862a172b875fff6830801
Anton <penitto02@gmail.com>
2020-03-15 18:06:11 +0300

    Производственный календарь и model.py

---
+++ model.py
@@ -0,0 +1,11 @@
+import datetime
+import numpy as np
+import pandas as pd
+import json
+
+
+def is_weekday(date):
+    with open("prod_cal.json", "r") as read_file:
+        holidays = json.load(read_file)
+    return True if (date.weekday() == 5) or (date.weekday() == 6) or (date.date().isoformat() in holidays[str(date.year)]) else False
+
