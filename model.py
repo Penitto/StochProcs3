f19cfec396ba5730a1d812474668212dde82f7da
Anton <penitto02@gmail.com>
2020-03-16 00:26:33 +0300

    Distribution test

--- model.py
+++ model.py
@@ -2,6 +2,7 @@ import datetime
 import numpy as np
 import pandas as pd
 import json
+import matplotlib.pyplot as plt
 
 
 def is_weekday(date):
@@ -9,3 +10,13 @@ def is_weekday(date):
         holidays = json.load(read_file)
     return True if (date.weekday() == 5) or (date.weekday() == 6) or (date.date().isoformat() in holidays[str(date.year)]) else False
 
+def plotMovingAverage(df, series, n):
+    rolling_mean = series.rolling(window=n).mean()
+
+    plt.figure(figsize=(15,5))
+    plt.title("Moving average\n window size = {}".format(n))
+    plt.plot(rolling_mean, "g", label="Rolling mean trend")
+
+    plt.plot(df[n:], label="Actual values")
+    plt.legend(loc="upper left")
+    plt.grid(True)
