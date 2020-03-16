d4cc77f4d17a664bdea78d0e28ad2c461cfec53f
Anton <penitto02@gmail.com>
2020-03-16 10:40:24 +0300

    Tests

--- model.py
+++ model.py
@@ -10,6 +10,16 @@ def is_weekday(date):
         holidays = json.load(read_file)
     return True if (date.weekday() == 5) or (date.weekday() == 6) or (date.date().isoformat() in holidays[str(date.year)]) else False
 
+def plotMovingAverage(df, series, n):
+    rolling_mean = series.rolling(window=n).mean()
+
+    plt.figure(figsize=(15,5))
+    plt.title("Moving average\n window size = {}".format(n))
+    plt.plot(rolling_mean, "g", label="Rolling mean trend")
+
+    plt.plot(df[n:], label="Actual values")
+    plt.legend(loc="upper left")
+    plt.grid(True)
 
 def call_script(data_file_path, date):
     pass 
@@ -46,14 +56,3 @@ def calibrate_hyper(df, model, model_spec):
 
 def get_data():
     return df
-
-def plotMovingAverage(df, series, n):
-    rolling_mean = series.rolling(window=n).mean()
-
-    plt.figure(figsize=(15,5))
-    plt.title("Moving average\n window size = {}".format(n))
-    plt.plot(rolling_mean, "g", label="Rolling mean trend")
-
-    plt.plot(df[n:], label="Actual values")
-    plt.legend(loc="upper left")
-    plt.grid(True)
