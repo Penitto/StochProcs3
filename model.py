6978dd3e2913357e4b59d33bfe39b60259cac5c8
Sunamur <lsherstyuk@gmail.com>
2020-03-22 21:40:22 +0300

    bug fixes, added ensemble, more logging

--- model.py
+++ model.py
@@ -11,6 +11,8 @@ from sklearn.model_selection import GridSearchCV, TimeSeriesSplit
 from sklearn.ensemble import RandomForestRegressor
 from lightgbm import LGBMRegressor
 import logging 
+import warnings
+warnings.filterwarnings('ignore')
 
 def get_logger():
     # From Official documentation
@@ -45,7 +47,7 @@ def get_change_point(series, jump=5, n_bkps=5, pen=10):
     pen: пенальти для Pelt
 
     """
-
+    series = series.values
     alg_dynp = rpt.Dynp(jump=jump).fit_predict(series, n_bkps=n_bkps)
 
     alg_pelt = rpt.Pelt(jump=jump).fit_predict(series, pen=pen)
@@ -81,28 +83,34 @@ def is_change_point(series, jump=5, n_bkps=5, pen=10, lim=10):
     res = get_change_point(series, jump, n_bkps, pen)
     return res[-1] >= len(series) - lim
 
-def get_model(data, target):
-    params1 = {'alpha' : np.linspace(0.0001, 1, 100), 
-               'l1_ratio' : np.linspace(0, 1, 100)}
+def get_model(data, target,use_ensemble=True):
+    params1 = {'alpha' : np.logspace(0.0001, 1, 30), 
+               'l1_ratio' : np.linspace(0, 1, 10)}
     
     params2 = {'n_estimators' : range(10, 101, 10),
                'max_depth' : range(2,10)}
 
-    params3 = {'learning_rate' : np.linspace(0.0001, 0.3, 50),
-               'n_estimators' : range(10, 101, 10),
-               'max_depth' : range(2, 10)}
-
-    el = ElasticNet()
-    gr_el = GridSearchCV(el, params1, cv=TimeSeriesSplit(), scoring='neg_mean_error',refit=True)
-    gr_el.fit(data, target)
-
-    rf = RandomForestRegressor()
-    gr_rf = GridSearchCV(rf, params2, cv=TimeSeriesSplit(), scoring='neg_mean_error',refit=True)
-    gr_rf.fit(data, target)
+    params3 = {'learning_rate' : np.logspace(0.0001, 0.5, 10),
+               'n_estimators' : range(10, 101, 30),
+               'max_depth' : [6,9,12],
+               'verbose':[-1]}
 
     lgb = LGBMRegressor()
-    gr_lgb = GridSearchCV(lgb, params3, cv=TimeSeriesSplit(), scoring='neg_mean_error',refit=True)
+    
+    gr_lgb = GridSearchCV(lgb, params3, cv=TimeSeriesSplit(), scoring='neg_mean_squared_error',refit=True)
     gr_lgb.fit(data, target)
+    logger.info('Booster params discovered')
+
+    el = ElasticNet(max_iter=5000)
+    gr_el = GridSearchCV(el, params1, cv=TimeSeriesSplit(), scoring='neg_mean_squared_error',refit=True)
+    gr_el.fit(data, target)
+    logger.info('ElasticNet params discovered')
+
+    rf = RandomForestRegressor()
+    gr_rf = GridSearchCV(rf, params2, cv=TimeSeriesSplit(), scoring='neg_mean_squared_error',refit=True)
+    gr_rf.fit(data, target)
+    logger.info('RandomForest params discovered')
+
 
     res_scores = {'elastic' : gr_el.best_score_, 
                   'random_forest' : gr_rf.best_score_,
@@ -111,7 +119,23 @@ def get_model(data, target):
     res_est = {'elastic' : gr_el.best_estimator_, 
                'random_forest' : gr_rf.best_estimator_,
                'lgbm' : gr_lgb.best_estimator_}
+    if use_ensemble:
+        estimators = [
+                    ('elastic', ElasticNet(**gr_el.best_params_)), 
+                    ('random_forest', RandomForestRegressor(**gr_rf.best_params_)),
+                    ('lgbm', LGBMRegressor(**gr_lgb.best_params_))]
 
+
+        from sklearn.ensemble import StackingRegressor
+
+
+        stacked = StackingRegressor(
+            estimators=estimators,
+            final_estimator=RandomForestRegressor(n_estimators=30),
+            passthrough=True)
+        stacked.fit(data, target)
+        logger.info('Ensemble fitted')
+        return stacked
     return res_est[sorted(res_scores, key=lambda x: (-res_scores[x], x))[0]]
 
 def is_weekday(date):
@@ -155,31 +179,21 @@ def select_features(
     df,#датафрейм со всеми фичами, в т ч генерированными
     target, #серия с таргетом
     ):
-    y = df.pop('target')
-    #логика
-    selected_features=[]
-    return selected_features
+    return df.columns
 
 
 
-def calibrate_hyper(
-    df,
-    target,
-    model):
-    model_spec = get_model_spec()
-    trained_model=None
-    return trained_model
 
 
-def get_data(date_until,target_data_file='project_3_train+test.xlsx'):
+def get_data(target_data_file='project_3_train+test.xlsx'):
     base_dates = get_dates_list(target_data_file)
-    if base_dates.max()>date_until:
-        base_dates = base_dates[base_dates<=date_until]
+    # if base_dates.max()>date_until:
+    #     base_dates = base_dates[base_dates<=date_until]
     base_df=pd.DataFrame(index=base_dates)
     
     target = pd.read_excel(target_data_file)
     target=target.set_index('Date')
-    target=target.loc[base_df.index]
+    target=target.loc[base_df.index].iloc[:,0]
 
     
     
@@ -199,7 +213,7 @@ def get_data(date_until,target_data_file='project_3_train+test.xlsx'):
     data_usdrub1 = data_usdrub[['Date', 'USDRUB']]
     data_brent1 = data_brent[['Date', 'BRENT']]
 
-    data_keyrate = pd.read_excel('keyrate_cbr.xlsx').iloc[::-1].reset_index(drop=True)
+    data_keyrate = pd.read_excel('keyrate_cbr.xlsx').iloc[::-1].replace({'—':np.nan}).reset_index(drop=True)
     data_liquidity = pd.read_excel('liquidity_cbr.xlsx').iloc[::-1].reset_index(drop=True)
     
     data_keyrate = data_keyrate.rename(columns={'Дата':'Date'})
@@ -212,22 +226,16 @@ def get_data(date_until,target_data_file='project_3_train+test.xlsx'):
         data_keyrate.set_index('Date'),
         data_liquidity.set_index('Date'),
     ], how='left')
-    
-
     econ_data = econ_data.interpolate(method='polynomial', order=3)
     return econ_data, target
 
-
-def get_model_spec():
-    spec=dict()
-    return spec
 
 
 
 def report_metric(
     target, 
     predictions):
-    metric = target-predictions
+    metric = np.abs(target-predictions)
     print(f'metric is {metric.mean()}')
     return metric
 
@@ -238,30 +246,37 @@ PREDICTIONS_FILEPATH='predictions.xlsx'
 
 def prepare_complete_model_and_data(date,target_data_file):
 
-    data, target = get_data(date, target_data_file)
+    data, target = get_data(target_data_file)
+    logger.info('Raw data fetched')
     full_data = generate_features(data)
+    logger.info('New features generated')
     selected_features = select_features(
         full_data.loc[:date],
         target.loc[   :date],)
     clean_data = full_data.loc[:,selected_features]
+    logger.info('Data selected')
     model = get_model(
-        clean_data.loc[:data],
-        target.loc[    :data])
+        clean_data.loc[:date],
+        target.loc[    :date])
     return model, clean_data, target
 
 
 
-def general_loop(target_data_file):
+def general_loop(target_data_file='project_3_train+test.xlsx'):
+    logger.info('Script started!')
     datelist = get_dates_list(target_data_file)
     start_date = datelist[STARTING_TICK]
     model, data, target = prepare_complete_model_and_data(start_date,target_data_file)
     logger.info('Initial model and data are ready')
-    predictions = pd.Series(index=target.index)
-    for day_count, cur_date in enumerate(datelist[STARTING_TICK+1:]):
-        predictions[cur_date] = model.predict(data.loc[cur_date])
+    predictions = pd.Series(index=datelist)
+    for day_count, cur_date in enumerate(datelist.values[STARTING_TICK+1:]):
+        predictions.loc[cur_date] = model.predict(data.loc[cur_date].values.reshape(1,-1))
         predictions.to_excel(PREDICTIONS_FILEPATH)
+        logger.info(f'Day {day_count} predicted, absolute error of {np.abs(predictions.loc[cur_date]-target.loc[cur_date])}')
         if is_change_point(target[:cur_date]):
             logger.info(f'Change-point detected on {cur_date}')
             model, data, target = prepare_complete_model_and_data(cur_date, target_data_file)
     metric_results = report_metric(target, predictions)
 
+if __name__=='__main__':
+    general_loop()
